676a63d5d2ffc05a4b22322493efbf8cefe81b95
anurag anand <anurag.anand@metacube.com>
2015-10-21 17:19:52 +0530

    Design Pattern Session 1

---
+++ Web/DesignPattern/src/Assignment2/Developer.java
@@ -0,0 +1,48 @@
+package Assignment2;
+/**
+ * Name: Developer
+ * @author Anurag
+ * Since: 21 October,2015
+ * Description: Modal Class of Developer
+ **/
+public class Developer 
+{
+	// To store name
+	String name;
+	// To store role
+	String role;
+
+	// Gets the Name
+	public String getName()
+	{
+		return name;
+	}
+	// Sets The Name
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	// Gets the role
+	public String getRole()
+	{
+		return role;
+	}
+	// Protected Constructor
+	protected Developer() 
+	{
+
+	}
+	// Sets The role
+	public void setRole(String role)
+	{
+		this.role = role;
+	}
+	// Overrides the To string method
+	@Override
+	public String toString() 
+	{
+		// TODO Auto-generated method stub
+		return "Name -> " + name + "\nRole -> " + role + "\n";
+	}
+
+}
